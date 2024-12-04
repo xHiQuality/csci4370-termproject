42dfbd058a5c29bc0f38f62007987e7cc1df5c00
Cason Pittman <xhiquality@gmail.com>
2024-12-03 20:18:17 -0500

    Added top 3 caps and goal scorers

--- worldcup/src/backend/routes/squads_2022s.js
+++ worldcup/src/backend/routes/squads_2022s.js
@@ -8,8 +8,7 @@ router.get('/:team', async (req, res) => {
 
     try {
         pool.query(
-            `
-            SELECT Player, Position, Age, Caps, Goals 
+            `SELECT Player, Position, Age, Caps, Goals 
             FROM squads_2022s WHERE Team = ? 
             ORDER BY 
                 CASE 
@@ -19,13 +18,64 @@ router.get('/:team', async (req, res) => {
                     WHEN Position = 'Goalkeeper' THEN 4
                     ELSE 5
                 END,
-                Player ASC
-            `,
+                Player ASC`,
             [team],
             (err, results) => {
                 if (err) {
                     console.error('Database error:', err);
                     return res.status(500).json({ error: 'Failed to fetch players' });
+                }
+                res.json(results);
+            }
+        );
+    } catch (error) {
+        console.error('Unexpected error:', error);
+        res.status(500).json({ error: 'Server error' });
+    }
+});
+
+// Route: Get top 3 goal scorers of a selected team
+router.get('/:team/top-scorers', async (req, res) => {
+    const { team } = req.params;
+
+    try {
+        pool.query(
+            `SELECT Player, Goals 
+            FROM squads_2022s 
+            WHERE Team = ? 
+            ORDER BY Goals DESC 
+            LIMIT 3`,
+            [team],
+            (err, results) => {
+                if (err) {
+                    console.error('Database error:', err);
+                    return res.status(500).json({ error: 'Failed to fetch top scorers' });
+                }
+                res.json(results);
+            }
+        );
+    } catch (error) {
+        console.error('Unexpected error:', error);
+        res.status(500).json({ error: 'Server error' });
+    }
+});
+
+// Route: Get top 3 country apperances of a selected team
+router.get('/:team/caps', async (req, res) => {
+    const { team } = req.params;
+
+    try {
+        pool.query(
+            `SELECT Player, Caps 
+            FROM squads_2022s 
+            WHERE Team = ? 
+            ORDER BY Caps DESC 
+            LIMIT 3`,
+            [team],
+            (err, results) => {
+                if (err) {
+                    console.error('Database error:', err);
+                    return res.status(500).json({ error: 'Failed to fetch top scorers' });
                 }
                 res.json(results);
             }
