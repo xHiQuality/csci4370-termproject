41bbed7888b8044cec0833315e2a9f6a329207d0
Cason Pittman <xhiquality@gmail.com>
2024-12-03 19:44:41 -0500

    Added get flag call

--- worldcup/src/backend/routes/teams.js
+++ worldcup/src/backend/routes/teams.js
@@ -2,7 +2,7 @@ const express = require('express');
 const pool = require('../db/connection.js');
 const router = express.Router();
 
-// Get all teams
+// Route: get all country names
 router.get('/', async (req, res) => {
     try {
         pool.query('SELECT country_name FROM teams', (err, results) => {
@@ -17,5 +17,31 @@ router.get('/', async (req, res) => {
         res.status(500).json({ error: 'Server error' });
     }
 });
+
+//Route: get flag of selected country
+router.get('/:country/flag', async(req,res) => {
+    const { country } = req.params; //get from URL
+
+    try {
+        pool.query(
+            'SELECT flag FROM teams WHERE country_name = ?',
+            [country],
+            (err, results) => {
+                if (err) {
+                    console.error('Database error:',err);
+                    return res.status(500).json({error: 'Failed to fetch flag'})
+                }
+                if (results.length === 0) {
+                    return res.status(404).json({error: 'Country not found'})
+                }
+                res.json(results[0])
+            }
+        )
+    } catch (error) {
+        console.error('Unexpected error:', error);
+        res.status(500).json({error: 'Server error'});
+    }
+});
+
 
 module.exports = router;
