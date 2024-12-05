54f6c2019939009c6ad62585b4166c989634f52b
Cason Pittman <xhiquality@gmail.com>
2024-12-04 19:51:53 -0500

    changes

--- worldcup/src/backend/db/connection.js
+++ worldcup/src/backend/db/connection.js
@@ -3,6 +3,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
+    password: 'admin',
     password: 'samantha1234',
     database: 'world_cup',
     waitForConnections: true,
